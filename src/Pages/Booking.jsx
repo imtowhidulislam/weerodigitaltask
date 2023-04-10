c339f01c80752f8a66778e38d51199e845e328aa
Towhid <niamaaktar@gmail.com>
2023-04-11 00:53:40 +0600

    Home page is Complete

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Booking = () => {
   return (
     <div className='flex items-center justify-center'>
-      <div className='container h-full p-12 text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tl from-purple-400 to-sky-600 mb-4 text-left'>
-        I'm kidda confuse about the content of the Booking Page that's why I couldn't able to accomplish this page.
+      <div className='container min-h-full px-12 py-20 text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-tl from-purple-400 to-sky-600 mb-4 text-left'>
+        I was kidda confuse about the content of the Booking Page that's why I couldn't able to accomplish this page.
       </div>
     </div>
   )
