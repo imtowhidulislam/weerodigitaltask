1c779fd20430c7b7af24f91df49ad1b98cfa7ece
Towhid <niamaaktar@gmail.com>
2023-04-10 23:51:49 +0600

    navbar issue gets resolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import {Link} from "react-router-dom"
 import {GiMagicLamp} from "react-icons/gi"
 const Navbar = () => {
   return (
-    <div className='bg-gray-400 navContainer py-2 px-8 lg:px-0'>
-        <div className='flex items-center justify-between'>
+    <div className='bg-gray-400 py-2 px-8 lg:px-0'>
+        <div className='flex items-center justify-between navContainer'>
             <div className='text-5xl text-gray-50'>
                 <GiMagicLamp />
             </div>
             <nav className="list-none flex items-center navbar">
-                <li >
+                <li>
                     <Link className='link' to="/">
                         Home
                     </Link>
